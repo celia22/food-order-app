3c78254497ee4a286fdfab5432a854f28af9add1
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-10-13 23:45:15 +0200

    added queries in Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,42 +1,37 @@
 import { context } from "../Context/apiProvider";
 import { useContext, useEffect } from "react";
 import Calendar from "../Components/Calendar/Calendar";
-import { useMutation, useQuery } from "react-query";
+import {useMutation, useQueries, useQuery} from "react-query";
 import axios from "../axios/axios";
 
 
 const Home = (props) => {
 	const apiContext = useContext(context);
 
-	const {
-		isLoading: bookingsIsLoading,
-		error: bookingsError,
-		data: bookingsData,
-		refetch: bookingsRefetch
-	} = useQuery(
-		["bookings"],
-		() => axios.get(`center/bookings/${apiContext.data.data._id}`),
+	const data = useQueries(
+		[
+			{
+				queryKey: ["bookings", apiContext.data?.data?._id],
+				queryFn: () => axios.get(`center/bookings/${apiContext.data?.data?._id}`),
+			},
+			{
+				queryKey: ["Center Services", apiContext?.data?.data?._id],
+				queryFn: () => axios.get(`/center/services/${apiContext.data?.data?._id}`)
+			},
+			{
+				queryKey: ["Center Employees", apiContext.data?.data?._id],
+				queryFn: () => axios.get(`/center/employees/${apiContext.data?.data?._id}`)
+			}
+		],
 		{
 			enabled: false,
-			onSuccess: () => console.log(bookingsData.data)
+			onSuccess: () => console.log(data)
 		}
-	);
-
-	const updateUserProfile = useMutation(
-		(data) => {
-			return axios.put(
-				`/user/update/${""}`,
-				data
-			);
-		},
-		{
-			onError: (error) => console.error(error),
-		}
-	);
+	)
 
 	useEffect(() => {
 		return () => {
-			apiContext.data && bookingsRefetch()
+			apiContext.data.data && data.forEach(query => query.refetch())
 		};
 	}, [apiContext.data]);
 
